97f30579e282bd189dfc276689f3987f0e7891e2
Fritz Lekschas <service@lekschas.de>
2016-03-10 22:39:42 -0500

    Working node context menu.

--- src/scripts/composition/nodeContextMenu.js
+++ src/scripts/composition/nodeContextMenu.js
@@ -1,23 +1,36 @@
+// External
+import Promise from '../../../node_modules/es6-promise/lib/es6-promise/Promise';
+
 // Internal
 import { dropMenu } from '../commons/charts';
+import { requestNextAnimationFrame } from '../commons/shims';
 
 const CLASS_NAME = 'context-menu';
+const CLASS_CHECKBOX = 'checkbox';
+const TRANSITION_SPEED = 125;
 
 class NodeContextMenu {
   constructor (vis, visData, baseEl, events, querying) {
-    // const that = this;
+    const that = this;
+
+    this._x = 0;
+    this._y = 0;
+    this._yOffset = 0;
+    this._scale = 0;
 
     this.vis = vis;
     this.visData = visData;
     this.baseEl = baseEl;
     this.events = events;
 
-    this.el = this.baseEl.append('g').attr('class', CLASS_NAME);
+    this.wrapper = this.baseEl.append('g')
+      .attr('class', CLASS_NAME)
+      .call(this.updateAppearance.bind(this));
 
     this.numButtonRows = querying ? 2 : 3;
     this.height = this.visData.global.row.height * this.numButtonRows;
 
-    this.bg = this.el.append('path')
+    this.bg = this.wrapper.append('path')
       .attr('class', 'bgBorder')
       .attr('d', dropMenu({
         x: -1,
@@ -25,11 +38,11 @@ class NodeContextMenu {
         width: this.visData.global.column.width + 2,
         height: this.height + 2,
         radius: 5,
-        arrowSize: 7
+        arrowSize: 6
       }))
       .style('filter', 'url(#drop-shadow-context-menu)');
 
-    this.bg = this.el.append('path')
+    this.bg = this.wrapper.append('path')
       .attr('class', 'bg')
       .attr('d', dropMenu({
         x: 0,
@@ -41,32 +54,44 @@ class NodeContextMenu {
       }))
       .style('filter', 'url(#drop-shadow-context-menu)');
 
-    this.buttonQuery = this.el.append('g')
+    this.buttonQuery = this.wrapper.append('g')
       .call(this.createButton.bind(this), {
         alignRight: false,
         classNames: [],
         distanceFromCenter: 1,
         fullWidth: true,
-        label: 'Query'
+        label: 'Query:',
+        labelTwo: 'query-mode'
+      })
+      .on('click', function () {
+        that.clickQueryHandler.call(that, this);
       });
 
-    this.buttonRoot = this.el.append('g')
+    this.buttonRoot = this.wrapper.append('g')
       .call(this.createButton.bind(this), {
         alignRight: false,
         classNames: [],
         distanceFromCenter: 0,
         fullWidth: false,
         label: 'Root'
+      })
+      .on('click', function () {
+        that.clickRootHandler.call(that, this);
       });
+    this.checkboxRoot = this.createCheckbox(this.buttonRoot);
 
-    this.buttonLock = this.el.append('g')
+    this.buttonLock = this.wrapper.append('g')
       .call(this.createButton.bind(this), {
         alignRight: true,
         classNames: [],
         distanceFromCenter: 0,
         fullWidth: false,
         label: 'Focus'
+      })
+      .on('click', function () {
+        that.clickLockHandler.call(that, this);
       });
+    this.checkboxLock = this.createCheckbox(this.buttonLock);
   }
 
   createButton (selection, properties) {
@@ -77,13 +102,15 @@ class NodeContextMenu {
     selection.attr('class', classNames);
 
     selection
+      .datum(properties)
       .call(
         this.createButtonBg.bind(this),
         properties.fullWidth
       )
       .call(
         this.addLabel.bind(this),
-        properties.label
+        properties.label,
+        properties.labelTwo
       )
       .call(
         this.positionButton.bind(this),
@@ -92,17 +119,66 @@ class NodeContextMenu {
       );
   }
 
+  clickQueryHandler () {
+    this.vis.nodes.toggleQueryMode(this.node);
+    this.updateQuery();
+  }
+
+  clickRootHandler () {
+    this.vis.nodes.toggleRoot(this.node);
+    this.close();
+  }
+
+  clickLockHandler () {
+    this.vis.nodes.toggleLock(this.node);
+    this.checkLock();
+  }
+
   createButtonBg (selection, fullWidth) {
-    selection.append('rect').attr({
-      class: 'bg',
-      x: this.visData.global.row.padding,
-      y: this.visData.global.row.padding,
-      width: this.visData.global.column.width * (fullWidth ? 1 : 0.5) -
-        this.visData.global.row.padding * 2,
-      height: this.visData.global.row.contentHeight,
-      rx: 2,
-      ry: 2
-    });
+    selection.datum(data => {
+      data.x = this.visData.global.row.padding;
+      data.y = this.visData.global.row.padding;
+      data.width = this.visData.global.column.width * (fullWidth ? 1 : 0.5) -
+        this.visData.global.row.padding * 2;
+      data.height = this.visData.global.row.contentHeight;
+      data.rx = 2;
+      data.ry = 2;
+
+      return data;
+    }).append('rect')
+      .attr('class', 'bg')
+      .attr('x', data => data.x)
+      .attr('y', data => data.y)
+      .attr('width', data => data.width)
+      .attr('height', data => data.height)
+      .attr('rx', data => data.rx)
+      .attr('ry', data => data.ry);
+  }
+
+  createCheckbox (selection) {
+    const height = Math.round(selection.datum().height / 2);
+    const x = -1.75 * height + this.visData.global.row.padding;
+
+    const container = selection.append('g').attr('class', CLASS_CHECKBOX);
+
+    container.append('rect')
+      .attr('class', 'checkbox-bg')
+      .attr('x', data => data.width + x)
+      .attr('y', height / 2 + this.visData.global.row.padding)
+      .attr('width', height * 1.5)
+      .attr('height', height)
+      .attr('rx', height / 2)
+      .attr('ry', height / 2);
+
+    this.checkBoxMovement = (height - 2) / 2;
+
+    return container.append('rect')
+      .attr('class', 'checkbox-knob')
+      .attr('x', data => data.width + x + 1)
+      .attr('y', height / 2 + this.visData.global.row.padding + 1)
+      .attr('width', height - 2)
+      .attr('height', height - 2)
+      .attr('rx', height - 2);
   }
 
   positionButton (selection, distanceFromCenter, alignRight) {
@@ -115,8 +191,8 @@ class NodeContextMenu {
     selection.attr('transform', `translate(${x}, ${y})`);
   }
 
-  addLabel (selection, label) {
-    selection.append('foreignObject')
+  addLabel (selection, label, labelTwo) {
+    const div = selection.append('foreignObject')
       .attr('x', this.visData.global.row.padding * 2)
       .attr('y', this.visData.global.row.padding +
         this.visData.global.cell.padding)
@@ -125,12 +201,136 @@ class NodeContextMenu {
         this.visData.global.cell.padding * 2)
       .attr('class', 'label-wrapper')
       .append('xhtml:div')
-        .attr('class', 'label')
-        .attr('title', label)
         .style('line-height', (this.visData.global.row.contentHeight -
-          this.visData.global.cell.padding * 2) + 'px')
-        .append('xhtml:span')
-          .text(label);
+          this.visData.global.cell.padding * 2) + 'px');
+
+    div.append('xhtml:span')
+      .attr('class', 'label')
+      .attr('title', label)
+      .text(label);
+
+    if (labelTwo) {
+      div.append('xhtml:span').attr('class', `label-two ${labelTwo}`);
+    }
+  }
+
+  get translate () {
+    return 'translate(' + this._x + 'px,' + (this._y + this._yOffset) + 'px)';
+  }
+
+  set translate (position) {
+    this._x = position.x;
+    this._y = position.y;
+  }
+
+  get scale () {
+    return 'scale(' + (this.opened ? 1 : 0.5) + ')';
+  }
+
+  updateAppearance (selection) {
+    selection
+      .classed('transitionable', this.visible)
+      .classed('open', this.opened)
+      .style('transform', this.translate + ' ' + this.scale)
+      .style(
+        'transform-origin',
+        (this.visData.global.column.width / 2) + 'px ' +
+        (this.height + this.visData.global.row.height) + 'px'
+      );
+  }
+
+  open (node) {
+    return new Promise(resolve => {
+      this.node = node;
+
+      this.updateStates();
+      this.translate = {
+        x: this.node.datum().x,
+        y: this.node.datum().y - this.height
+      };
+      this._yOffset = this.visData.nodes[this.node.datum().depth].scrollTop;
+      this.wrapper.call(this.updateAppearance.bind(this));
+      this.opened = true;
+      this.visible = true;
+
+      requestNextAnimationFrame(() => {
+        this.wrapper.call(this.updateAppearance.bind(this));
+        setTimeout(() => { resolve(true); }, TRANSITION_SPEED);
+      });
+    });
+  }
+
+  close () {
+    return new Promise(resolve => {
+      this.opened = false;
+      this.wrapper.call(this.updateAppearance.bind(this));
+
+      setTimeout(() => {
+        this.visible = false;
+        this.wrapper.call(this.updateAppearance.bind(this));
+        resolve(this.node.datum().id);
+        this.node = undefined;
+      }, TRANSITION_SPEED);
+    });
+  }
+
+  toggle (node) {
+    return new Promise(resolve => {
+      const nodeId = node.datum().id;
+
+      let closed = Promise.resolve();
+
+      if (this.visible) {
+        closed = this.close();
+      }
+
+      closed.then((previousNodeId) => {
+        if (nodeId !== previousNodeId) {
+          this.open(node).then(() => { resolve(nodeId); });
+        } else {
+          resolve(nodeId);
+        }
+      });
+    });
+  }
+
+  scrollY (offset) {
+    this._yOffset = offset;
+    this.wrapper.call(this.updateAppearance.bind(this));
+  }
+
+  updateStates () {
+    this.checkLock();
+    this.checkRoot();
+    this.updateQuery();
+  }
+
+  checkLock () {
+    const checked = this.node.datum().data.state.lock;
+    this.buttonLock.classed('active', checked);
+    this.checkboxLock.style(
+      'transform',
+      'translateX(' + (checked ? this.checkBoxMovement : 0) + 'px)'
+    );
+  }
+
+  checkRoot () {
+    const checked = this.node.datum().data.state.root;
+    this.buttonRoot.classed('active', checked);
+    this.checkboxRoot.style(
+      'transform',
+      'translateX(' + (checked ? this.checkBoxMovement : 0) + 'px)'
+    );
+  }
+
+  updateQuery () {
+    const queryMode = this.node.datum().data.state.query;
+
+    this.buttonQuery
+      .classed('active', !!queryMode)
+      .select('.query-mode')
+        .text(queryMode || 'not queried')
+        .classed('inactive', !queryMode);
   }
 }
 
