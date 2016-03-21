ea54a5fb54c2a30d393a06c5448d7be35a6a2ea9
Fritz Lekschas <service@lekschas.de>
2016-03-21 10:15:16 -0400

    Refactor static path generating functions and make them a member of `bars`. Fix an issue with indicator bars close to the value of 0.

--- src/scripts/composition/bars.js
+++ src/scripts/composition/bars.js
@@ -4,6 +4,7 @@ import * as d3 from 'd3';
 // Internal
 import Bar from './bar';
 import * as config from './config';
+import { roundRect } from '../commons/charts';
 
 const BARS_CLASS = 'bars';
 
@@ -14,6 +15,10 @@ class Bars {
     this.vis = vis;
     this.mode = mode;
     this.visData = visData;
+
+    this.indicatorX = d3.scale.linear()
+      .domain([0, 1])
+      .range([1, this.visData.global.column.contentWidth - 3]);
 
     this.selection = selection.append('g').attr('class', BARS_CLASS);
 
@@ -27,7 +32,7 @@ class Bars {
       .data(update, data => data.barId)
       .transition()
       .duration(config.TRANSITION_SEMI_FAST)
-      .attr('d', data => Bar.generatePath(
+      .attr('d', data => this.generatePath(
         data, this.mode, sortBy, this.visData
       ));
   }
@@ -54,18 +59,13 @@ class Bars {
     selection.selectAll('.bar-magnitude')
       .transition()
       .duration(config.TRANSITION_SEMI_FAST)
-      .attr('d', data => Bar.generatePath(
-        data, this.mode, sortBy, this.visData
-      ));
+      .attr('d', data => this.generatePath(data, sortBy));
   }
 
-  updateIndicator (bars, referenceValue, direct) {
-    Bar.updateIndicator(
-      bars,
-      this.visData.global.column.contentWidth,
-      direct ? this.visData.global.row.contentHeight : 4,
-      referenceValue
-    );
+  updateIndicator (selection, referenceValue) {
+    selection
+      .attr('x', this.indicatorX(referenceValue))
+      .attr('width', 2);
   }
 
   switchMode (mode, currentSorting) {
@@ -75,8 +75,8 @@ class Bars {
           this.selection.selectAll('.bar').selectAll('.bar-magnitude')
             .transition()
             .duration(config.TRANSITION_SEMI_FAST)
-            .attr('d', data => Bar.generateOneBarPath(
-              data, currentSorting.global.type, this.visData
+            .attr('d', data => this.generateOneBarPath(
+              data, currentSorting.global.type
             ));
         } else {
           // console.error(
@@ -90,16 +90,98 @@ class Bars {
         this.selection.selectAll('.bar.precision').selectAll('.bar-magnitude')
           .transition()
           .duration(config.TRANSITION_SEMI_FAST)
-          .attr('d', data => Bar.generateTwoBarsPath(data, this.visData));
+          .attr('d', data => this.generateTwoBarsPath(data));
 
         this.selection.selectAll('.bar.recall').selectAll('.bar-magnitude')
           .transition()
           .duration(config.TRANSITION_SEMI_FAST)
-          .attr('d', data => Bar.generateTwoBarsPath(data, this.visData, true));
+          .attr('d', data => this.generateTwoBarsPath(data, true));
       }
 
       this.mode = mode;
     }
+  }
+
+  generatePath (
+    data, currentSorting, indicator, adjustWidth, bottom
+  ) {
+    if (this.mode === 'two') {
+      return this.generateTwoBarsPath(data, bottom);
+    }
+    return this.generateOneBarPath(
+      data, currentSorting, indicator, adjustWidth
+    );
+  }
+
+  generateOneBarPath (
+    data, currentSorting, indicator, adjustWidth
+  ) {
+    const height = this.visData.global.row.contentHeight;
+    const normValue = Math.min(data.value, 1);
+    const normIndicator = Math.min(indicator, 1);
+
+    let x = 0;
+    let width = 2;
+
+    let radius = {
+      topLeft: 2,
+      bottomLeft: 2
+    };
+
+    if (indicator) {
+      radius = {};
+    }
+
+    if (data.id !== currentSorting && typeof indicator === 'undefined') {
+      x = this.indicatorX(normValue);
+      radius = {};
+    } else if (indicator) {
+      x = normIndicator * this.visData.global.column.contentWidth;
+      if (adjustWidth) {
+        if (normValue < normIndicator) {
+          x = normValue * this.visData.global.column.contentWidth;
+        }
+        width = Math.max(
+          Math.abs(normIndicator - normValue) *
+            this.visData.global.column.contentWidth
+          , 2
+        );
+      }
+    } else {
+      width = this.visData.global.column.contentWidth * normValue;
+    }
+
+    x = Math.min(x, this.visData.global.column.contentWidth - 2);
+
+    return roundRect(
+      x,
+      this.visData.global.row.padding,
+      width,
+      height,
+      radius
+    );
+  }
+
+  generateTwoBarsPath (data, bottom) {
+    const normValue = Math.min(data.value, 1);
+    const height = this.visData.global.row.contentHeight / 2;
+    const width = this.visData.global.column.contentWidth * normValue;
+
+    let y = this.visData.global.row.padding;
+    let radius = { topLeft: 2 };
+
+    if (bottom) {
+      radius = { bottomLeft: 2 };
+      y += height;
+    }
+
+    return roundRect(
+      0,
+      y,
+      width,
+      height,
+      radius
+    );
   }
 }
 
