5625843307cb259cbdcd42fa75667e18cf5d289c
Fritz Lekschas <service@lekschas.de>
2016-09-14 10:54:22 -0400

    Documentation and removed unused configurations.

--- src/scripts/composition/config.js
+++ src/scripts/composition/config.js
@@ -1,33 +1,134 @@
 export const CLASSNAME = 'list-graph';
 
+/**
+ * Width of the scrollbar in pixel.
+ *
+ * @type  {Number}
+ */
 export const SCROLLBAR_WIDTH = 6;
+
+/**
+ * Default number of columns
+ *
+ * @type  {Number}
+ */
 export const COLUMNS = 5;
+
+/**
+ * Default number of rows.
+ *
+ * @type  {Number}
+ */
 export const ROWS = 5;
 
-// An empty path is equal to inline SVG.
+/**
+ * Path to the icon file. An empty path is equal to inline SVG.
+ *
+ * @type  {String}
+ */
 export const ICON_PATH = '';
 
-// -1 = desc, 1 = asc
+/**
+ * Default sort order.
+ *
+ * @description
+ * -1 = desc, 1 = asc
+ *
+ * @type  {Number}
+ */
 export const DEFAULT_SORT_ORDER = -1;
 
+/**
+ * Default bar display mode.
+ *
+ * @type  {String}
+ */
 export const DEFAULT_BAR_MODE = 'one';
 
+/**
+ * Default highlighting of the active level.
+ *
+ * @type  {Boolean}
+ */
 export const HIGHLIGHT_ACTIVE_LEVEL = true;
+
+/**
+ * Default active level.
+ *
+ * @type  {Number}
+ */
 export const ACTIVE_LEVEL = 0;
+
+/**
+ * Default difference when no custom root is queried for.
+ *
+ * @type  {Number}
+ */
 export const NO_ROOT_ACTIVE_LEVEL_DIFF = 0;
+
+/**
+ * Default querying.
+ *
+ * @type  {Boolean}
+ */
 export const QUERYING = false;
+
+/**
+ * Default value for hiding links pointing to nodes outside the visible area.
+ *
+ * @type  {Boolean}
+ */
 export const HIDE_OUTWARDS_LINKS = false;
+
+/**
+ * Default value for showing the link indicator bar when links to hidden nodes
+ * are hidden.
+ *
+ * @type  {Boolean}
+ */
 export const SHOW_LINK_LOCATION = false;
+
+/**
+ * Default for disabling debouncing of the node context menu.
+ *
+ * @type  {Boolean}
+ */
 export const DISABLE_DEBOUNCED_CONTEXT_MENU = false;
 
+/**
+ * Default transition speed in milliseconds for super fast transition.
+ *
+ * @type  {Number}
+ */
 export const TRANSITION_LIGHTNING_FAST = 150;
-export const TRANSITION_FAST = 200;
-export const TRANSITION_SEMI_FAST = 250;
-export const TRANSITION_NORMAL = 333;
-export const TRANSITION_SLOW = 666;
-export const TRANSITION_SLOWEST = 1;
 
-// Gradient colors
-export const COLOR_ORANGE = '#f23e00';
-export const COLOR_NEGATIVE_RED = '#e0001c';
-export const COLOR_POSITIVE_GREEN = '#60bf00';
+/**
+ * Default transition speed in milliseconds for fast transition.
+ *
+ * @type  {Number}
+ */
+export const TRANSITION_FAST = 200;
+/**
+ * Default transition speed in milliseconds for semi-fast transition.
+ *
+ * @type  {Number}
+ */
+export const TRANSITION_SEMI_FAST = 250;
+/**
+ * Default transition speed in milliseconds for normal transition.
+ *
+ * @type  {Number}
+ */
+export const TRANSITION_NORMAL = 333;
+/**
+ * Default transition speed in milliseconds for slow transition.
+ *
+ * @type  {Number}
+ */
+export const TRANSITION_SLOW = 666;
+/**
+ * Default transition speed in milliseconds for slow transition.
+ *
+ * @type  {Number}
+ */
+export const TRANSITION_SLOWEST = 1;
